143a638fea8231861851bc56ee07c10436d6bcdb
Aksh4code <akkisingh8266@gmail.com>
2024-10-05 23:30:28 +0530

    created account routes

---
+++ backend/routes/account.js
@@ -0,0 +1,109 @@
+const express = require("express")
+const accountRouter = express.Router()
+const authMiddleware = require("../middlewares/user");
+const { Account } = require("../DB");
+const { default: mongoose } = require("mongoose");
+
+accountRouter.get('/balance', authMiddleware, async (req,res) => {
+
+    const account = await Account.findOne({
+        userId: req.userId
+    })
+
+    res.json({
+        balance : account.balance
+    })
+})
+
+
+// Good solution using transaction 
+accountRouter.post('/transfer', authMiddleware, async (req,res) => {
+
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const {to, ammount} = req.body;
+
+    const account = await Account.findOne({
+        userId: req.userId
+    }).session(session)
+
+    if (account.balance < ammount) {
+        session.abortTransaction();
+        res.status(400).json({ message : "Insufficient balance" })
+    }
+
+    const toAccount = await Account.findOne({
+        userId : to
+    }).session(session)
+    
+    if (!toAccount) {
+        session.abortTransaction();
+        res.status(400).json({ message : "Invalid account" })
+    }
+
+    await Account.updateOne({
+        userId: req.userId
+    },{
+        $inc : { balance : -ammount }
+    }).session(session)
+    
+    await Account.updateOne({
+        userId: to
+    },{
+        $inc : { balance : ammount }
+    }).session(session)
+
+    session.commitTransaction();
+    res.status(200).json({ message : "Transaction Successful" })
+
+})
+
+
+// Bad solution without using transaction if database goes down it wont work 
+// accountRouter.post('/transfer', authMiddleware, async (req,res) => {
+//     const {to, ammount} = req.body;
+
+//     const account = await Account.findOne({
+//         userId: req.userId
+//     })
+
+//     if (account.balance < ammount) {
+//         res.status(400).json({
+//             message : "Insufficient balance"
+//         })
+//     }
+
+//     const toAccount = await Account.findOne({
+//         userId : to
+//     })
+
+//     if (!toAccount) {
+//         res.status(400).json({
+//             message : "Invalid account" 
+//         })
+//     }
+
+//     await Account.updateOne({
+//         userId: req.userId
+//     },{
+//         $inc : {
+//             balance : -ammount
+//         }
+//     })
+    
+//     await Account.updateOne({
+//         userId: to
+//     },{
+//         $inc : {
+//             balance : ammount
+//         }
+//     })
+
+//     res.status(200).json({
+//         message : "Transaction Successful"
+//     })
+
+// })
+ 
+module.exports = accountRouter
